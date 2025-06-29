c8a562325b936259b5d6c50ae330b8d898fa7edf
HarshVileon <iamharshsingh@gmail.com>
2025-06-29 12:28:48 +0530

    Two more code added for java

---
+++ Solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Solution {
+    public static void solve() {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        System.out.println(a+" "+b);
+sc.close();
+    }
+}
---
+++ advance.java
@@ -0,0 +1,8 @@
+class advance {
+    public static void main(String[] args) {
+        double u= Math.PI;
+        float y= (float)Math.PI;
+        System.out.printf("Value of u is %.2f\n", u);
+        System.out.printf("Value of y is %.2f\n", y);
+    }
+}
